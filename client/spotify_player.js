e0d28202d61bbe174dae3df1749c56b3c1ac44f0
Kevin Pierce <piercekg@gmail.com>
2021-03-30 13:13:05 -0600

    Landslide Player!!!!

--- client/spotify_player.js
+++ client/spotify_player.js
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>MVP</title>
-</head>
-<body>
-  <script src="https://sdk.scdn.co/spotify-player.js"></script>
-  <script>
-  window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = 'BQCRrPekmeqfVMUgafj2nz2djfFSeAjFYOJDZnRgng7yYX3B8vXT0alwfyB8Yu2e1bjKy8J8iAgP35qlF_XXB8y-qxqT-OVz6H_aad27ybR343YQLJZonAoQ_Tt_xj5G0SnT8afndUFuuw-C_xh5ptruc4nV6x4c';
-
+        const token = 'BQCRrPekmeqfVMUgafj2nz2djfFSeAjFYOJDZnRgng7yYX3B8vXT0alwfyB8Yu2e1bjKy8J8iAgP35qlF_XXB8y-qxqT-OVz6H_aad27ybR343YQLJZonAoQ_Tt_xj5G0SnT8afndUFuuw-C_xh5ptruc4nV6x4c';
     const player = new Spotify.Player({
       name: 'Stevie Nicks Player',
       getOAuthToken: cb => { cb(token); },
@@ -27,12 +17,7 @@
 
     // Ready
     player.addListener('ready', ({ device_id }) => {
-      player._options.id = device_id;
       console.log('Ready with Device ID', device_id);
-      play({
-          playerInstance: player,
-          spotify_uri: 'spotify:track:5fprEY6WEN1wvFXkgfb22C',
-        });
     });
 
     // Not Ready
@@ -43,7 +28,6 @@
     // Connect to the player!
     player.connect().then(success => {
       if (success) {
-        console.log(player);
         console.log('The Web Playback SDK successfully connected to Spotify!');
       }
     })
@@ -53,14 +37,12 @@
       playerInstance: {
         _options: {
           getOAuthToken,
-          id,
-          name,
-          volume
+          id
         }
       }
     }) => {
       getOAuthToken(access_token => {
-        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device}`, {
           method: 'PUT',
           body: JSON.stringify({ uris: [spotify_uri] }),
           headers: {
@@ -71,9 +53,31 @@
       });
     };
 
-  };
+    play({
+      playerInstance: new Spotify.Player({
+          name: 'Stevie Nicks Player',
+          getOAuthToken: cb => { cb(token); },
+          id: '870474d0e3e2461d8551a0fd2d8348f3d7225f6e',
+          volume: 0.8,
+        }), spotify_uri: 'spotify:track:5fprEY6WEN1wvFXkgfb22C',
+    });
+
+<iframe id="sc-widget" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1848538&show_artwork=true"></iframe>
+  <script src="https://w.soundcloud.com/player/api.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    (function(){
+      var widgetIframe = document.getElementById('sc-widget'),
+          widget       = SC.Widget(widgetIframe),
+          newSoundUrl = 'https://api.soundcloud.com/tracks/13692671';
+
+      widget.bind(SC.Widget.Events.READY, function() {
+        // load new widget
+        widget.bind(SC.Widget.Events.FINISH, function() {
+          widget.load(newSoundUrl, {
+            show_artwork: false
+          });
+        });
+      });
+
+    }());
   </script>
-  <div id="app"></div>
-  <script src="app.js"></script>
-</body>
-</html>
